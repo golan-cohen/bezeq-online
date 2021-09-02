a3bad37010130e9e930c9d9ef9a271aa3a5ceaec
golan-cohen <golanco99@gmail.com>
2021-09-02 13:45:24 +0300

    golan

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const http = require("http");
-const port = 3000;
-
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
+
 const server = http.createServer(app);
 server.listen(port);
 
